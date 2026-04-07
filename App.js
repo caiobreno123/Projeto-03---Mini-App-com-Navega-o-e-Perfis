b248cd30862358ff4c0ad647023ef53ae67872e3
caiobreno123 <caiobrenopiresduarte@gmail.com>
2026-04-07 13:46:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import StackRoutes from './navigation/StackRoutes';
+export default function App() {
+  return <StackRoutes />;
+}
---
+++ navigation/TabRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from '../screens/Home';
+import Perfil from '../screens/Perfil';
+import Config from '../screens/Config';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes({ route }) {
+  const { user } = route.params;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: user === 'caio' ? '#6C63FF' : '#00C853'
+        },
+        headerTintColor: '#fff'
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} initialParams={{ user }} />
+      <Tab.Screen name="Perfil" component={Perfil} initialParams={{ user }} />
+      <Tab.Screen name="Configurações" component={Config} />
+    </Tab.Navigator>
+  );
+}
